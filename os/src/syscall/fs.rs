aa31cc56943a8c2a5ab934fae8947b9519170522
Fchen <fc_2012@163.com>
2024-10-24 14:40:27 +0000

    feat: 让代码跑起来

--- os/src/syscall/fs.rs
+++ os/src/syscall/fs.rs
@@ -1,11 +1,7 @@
 //! File and filesystem-related syscalls
 use crate::fs::{open_file, OpenFlags, Stat};
 use crate::mm::{translated_byte_buffer, translated_str, UserBuffer};
-use crate::task::{current_task, current_user_token,suspend_current_and_run_next};
-use crate::sbi::console_getchar;
-
-const FD_STDIN: usize = 0;
-const FD_STDOUT: usize = 1;
+use crate::task::{current_task, current_user_token};
 
 /// write buf of length `len`  to a file with `fd`
 pub fn sys_write(fd: usize, buf: *const u8, len: usize) -> isize {
